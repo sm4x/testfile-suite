dc44ae8475bce3b41dafa37f6cc99ab16ad67917
smax <max.schattauer@gmail.com>
2026-02-22 07:27:23 +0100

    Update main.py
    Add argument functionality

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
+import argparse
 import base64
+import sys
 
 def main():
-    pass
+    # Echo arguments passed to the script without standard argparse parsing for now
+    # to purely echo the parameters as requested
+    args = sys.argv[1:]
+    if args:
+        print(" ".join(args))
+    else:
+        print("No arguments provided.")
 
-# main function
 if __name__ == "__main__":
     main()
